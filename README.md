7df1e96a10e9f7adcb5f085f63c7caa4148e593b
Vulcor <Damnrottlet@gmail.com>
2025-06-19 21:31:38 +0300

    - add DB connection - partial dockerization

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ### Develop
 
+To be able to run local dev server a DB instance must be running in the background. For that run the following command:
+
+```
+docker-compose up -d
+```
+
 To develop all apps and packages, run the following command:
 
 ```
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.API_PORT ?? 3099;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.NEST_PORT ?? 3099);
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => {
+    console.log('App is running on port', port);
+  })
+  .catch((err) => {
+    console.error('Error starting the application:', err);
+  });
